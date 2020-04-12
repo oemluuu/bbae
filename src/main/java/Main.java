02d2b328682ea588759fae04c6534264d14399fb
Tohlester <Tohlester1@users.noreply.github.com>
2020-04-12 14:59:26 +0200

    Hehe boa

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Lass heute Abend coden");
+        System.out.println("Jiggo: 'We did boy, virginity is nomore!'");
     }
 }
